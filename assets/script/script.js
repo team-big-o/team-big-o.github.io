f80310fdbacfdd7f6e88f6d95c16b7c1548bd8e0
Akira Takaki <adude20801@gmail.com>
2019-03-23 18:30:36 -0400

    adding libraries, and fixing the mobile dropdown

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+console.log("ietaiga");
+window.onload = function(){
+    document.getElementById("burger").addEventListener( 'click', changeClass);
+}
+
+function changeClass() {
+    if (document.getElementById("burger").className.match(/(?:^|\s)is-active(?!\S)/)){
+        document.getElementById("burger").className = document.getElementById("burger").className.replace(/(?:^|\s)is-active(?!\S)/g , '' );
+        document.getElementById("navbarContent").className = document.getElementById("navbarContent").className.replace(/(?:^|\s)is-active(?!\S)/g , '' );
+    } else {
+        document.getElementById("burger").className += " is-active";
+        document.getElementById("navbarContent").className += " is-active";
+    }
+ }
