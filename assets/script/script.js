f3f1b3461bb9c42079d5cebf58c22f6eee235f9c
Akira Takaki <adude20801@gmail.com>
2019-03-24 00:28:22 -0400

    made some minor changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,3 @@
-window.onscroll = function() {myFunction()};
 window.onload = function(){
     document.getElementById("burger").addEventListener( 'click', changeClass);
 }
@@ -12,12 +11,3 @@ function changeClass() {
         document.getElementById("navbarContent").className += " is-active";
     }
  }
-
-function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-    } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-    }
-}
