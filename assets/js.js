197bb1068bc19fba8f9ce242bee4e99fbeb5b097
Jeffmagma <jeffrey.magmachu@gmail.com>
2019-03-24 01:00:57 -0400

    modular/procedural now

--- assets/js.js
+++ assets/js.js
@@ -1,26 +1,28 @@
 $(function() {
-	let $page = $('.page');
-	let $page1 = $('#page1');
-	let $page2 = $('#page2');
-	let $page3 = $('#page3');
-	$page.css('min-height', window.innerHeight);
-	$page.css('min-width', window.innerWidth);
+	let $pages_container = $('.pages_container');
+	let $pages = $pages_container.children('.page');
+
+	$pages.css('min-height', window.innerHeight);
+	$pages.css('min-width', window.innerWidth);
 
 	$(window).resize(() => {
-		$page.css('min-height', window.innerHeight);
-		$page.css('min-width', window.innerWidth);
+		$pages.css('min-height', window.innerHeight);
+		$pages.css('min-width', window.innerWidth);
 	});
 
 	let pos = 0;
-	let scrollto = [$page1.offset().top, $page2.offset().top, $page3.offset().top];
+	let scrollto = [];
+	$pages.each(function() {
+		console.log(this);
+		scrollto.push($(this).offset().top);
+	});
 	let scrolling = false;
-	$(window).on('mousewheel DOMMouseScroll', event => {
+	$(window).on('mousewheel', event => {
 		if (!scrolling) {
-			console.log(event.originalEvent.wheelDelta);
 			if (event.originalEvent.wheelDelta > 0) {
 				if (pos > 0) pos--;
 			} else if (event.originalEvent.wheelDelta < 0) {
-				if (pos < 2) pos++;
+				if (pos < scrollto.length) pos++;
 			}
 			if ($(window).scrollTop() - scrollto[pos]) {
 				scrolling = true;
