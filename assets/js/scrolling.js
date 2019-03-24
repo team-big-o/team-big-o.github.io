803502662eb53ec617c21301e886493aac33b755
Jeffmagma <jeffrey.magmachu@gmail.com>
2019-03-24 02:59:30 -0400

    no

--- assets/js/scrolling.js
+++ assets/js/scrolling.js
@@ -16,7 +16,7 @@ $(function() {
 		console.log(this);
 		scrollto.push($(this).offset().top);
 	});
-	if (scrollto[0] !== 0) scrolto.push(0);
+	if (scrollto[0] !== 0) scrollto.push(0);
 	let scrolling = false;
 	$(window).on('mousewheel', event => {
 		if (!scrolling) {
