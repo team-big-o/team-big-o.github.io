e00c021f6f5d9ae03b158c665345af2749d2d32b
Jeffmagma <jeffrey.magmachu@gmail.com>
2019-03-24 11:31:09 -0400

    snek

--- games/snek.js
+++ games/snek.js
@@ -5,17 +5,18 @@ $(() => {
 	let context = canvas.getContext('2d');
 
 	function rand(x, y) {
-		return (x + Math.random() * (y - x + 1));
+		return Math.floor(x + Math.random() * (y - x + 1));
 	}
 
 	function mod(n, m) {
 		return ((n % m) + m) % m;
 	}
 
-	let points, food, direction, to_add;
+	let points, food, direction, to_add, water;
 	function reset() {
 		points = [[25, 25]];
-		food = [Math.floor(rand(0, 49)), Math.floor(rand(0, 49))];
+		food = [rand(0, 49), rand(0, 49)];
+		water = [[rand(0, 49), rand(0, 49)]];
 		direction = 'right';
 		to_add = 3;
 	}
@@ -70,6 +71,7 @@ $(() => {
 			point[1] = mod(point[1], 50);
 			context.fillStyle = "gray";
 			context.fillRect(point[0] * 10, point[1] * 10, 10, 10);
+			// if touching self
 			if (i > 0 && point[0] === points[0][0] && point[1] === points[0][1]) {
 				alert('dead!');
 				reset();
@@ -78,13 +80,24 @@ $(() => {
 		// if touching food
 		if (points[0][0] === food[0] && points[0][1] === food[1]) {
 			to_add += 3;
-			food = [Math.floor(rand(0, 49)), Math.floor(rand(0, 49))];
+			food = [rand(0, 49), rand(0, 49)];
 		}
 		// draw the food and river
 		context.fillStyle = 'rgb(128, 128, 0)';
 		context.fillRect(food[0] * 10, food[1] * 10, 10, 10);
-		// if touching self
-
+		for (let w of water) {
+			w[0] = mod(w[0], 50);
+			w[1] = mod(w[1], 50);
+			if (points[0][0] === w[0] && points[0][1] === w[1]) {
+				alert('dead!');
+				reset();
+			}
+			context.fillStyle = "cyan";
+			context.fillRect(w[0] * 10, w[1] * 10, 10, 10);
+		}
+		let temp = water[rand(0, water.length - 1)];
+		let ty = rand(-1, 1), tx = rand(-1, 1);
+		water.push([temp[0] + tx, temp[1] + ty]);
 	}
 	reset();
 	setInterval(() => window.requestAnimationFrame(render), 100);
